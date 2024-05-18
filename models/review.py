09578414d246a2dcb01c8ded8ca780020d4b14a8
Chirchir-Dan <1danchirchir@gmail.com>
2024-05-18 17:49:44 +0300

    Added documentation

--- models/review.py
+++ models/review.py
@@ -5,10 +5,26 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review class inherits from BaseModel """
+    """
+    Review class inherits from BaseModel
+
+    Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+            'place_id', 'user_id', and 'text' are
+            retrieved from kwargs if provided.
+    """
 
     def __init__(self, *args, **kwargs):
-        """ Initializes a new Review instance. """
+        """
+        Initializes a new Review instance.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword argumeents.
+                'place_id', 'user_id', and 'text' are
+                retrieved from kwargs if provided.
+        """
         super().__init__(*args, **kwargs)
         self.place_id = kwargs.get('place_id', "")
         self.user_id = kwargs.get('user_id', "")
