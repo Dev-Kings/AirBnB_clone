69b94d222d77cf55b5344aa48d1c0ac5299b7ae2
Chirchir-Dan <1danchirchir@gmail.com>
2024-05-18 18:52:56 +0300

    Added Module documentation

--- models/place.py
+++ models/place.py
@@ -1,6 +1,35 @@
 #!/usr/bin/python3
 # models/place.py
 
+"""
+place.py
+
+This module defines the Place class, a subclass of
+BaseModel, which represents places that can be rented
+or listed in the application.
+
+Classes:
+    Place: A class that inherits from BaseModel and
+    represents a place with various attributes like name,
+    description, number of rooms, etc.
+
+Usage:
+    This module is typically used as part of a larger
+    application that manages different models like users,
+    places, states, cities, amenities, and reviews.
+
+Example:
+    from models.place import Place
+    new_place = Place(name="Cozy Cottage", city_id="1234",
+    user_id="5678", description="A cozy place to stay",
+    number_rooms=3, number_bathrooms=2, max_guest=4,
+    price_by_night=100)
+    new_place.save()
+
+Attributes:
+    (No module-level attributes)
+"""
+
 from models.base_model import BaseModel
 
 
